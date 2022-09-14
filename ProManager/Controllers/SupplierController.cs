5b8476cdcd2275486bec612f8a63d9b0496017ef
Juan Lafuente <jlafuente@telconet.ec>
2022-09-14 11:59:40 -0500

    se implementa las pantallas de empresas y contactos

---
+++ ProManager/Controllers/SupplierController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProManager.Controllers
+{
+    public class SupplierController : Controller
+    {
+        // GET: Supplier
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Supplier
+        public ActionResult Contacts()
+        {
+            return View();
+        }
+
+
+    }
+}
